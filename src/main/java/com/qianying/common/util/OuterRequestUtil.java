cdd8562f806034f62d94238c9ea0995d5de265c0
xieyuxing <185246042@qq.com>
2017-08-22 12:39:19 +0800

    little change

--- src/main/java/com/qianying/common/util/OuterRequestUtil.java
+++ src/main/java/com/qianying/common/util/OuterRequestUtil.java
@@ -1,20 +1,20 @@
 package com.qianying.common.util;
 
+/**
+ * Created by xieyuxing on 2017/8/22.
+ */
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Administrator on 2015/11/12.
- */
 public class OuterRequestUtil {
     public static String sendGet(String url) {
         String result = "";
@@ -31,12 +31,7 @@ public class OuterRequestUtil {
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 建立实际的连接
             connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-//                System.out.println(key + "--->" + map.get(key));
-            }
+
             // 定义 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(
                     connection.getInputStream()));
@@ -76,12 +71,7 @@ public class OuterRequestUtil {
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 建立实际的连接
             connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-//                System.out.println(key + "--->" + map.get(key));
-            }
+
             // 定义 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(
                     connection.getInputStream()));
@@ -105,7 +95,46 @@ public class OuterRequestUtil {
         }
         return result;
     }
+    public static String sendGet(String url, Map<String, String[]> params) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + getParamsString(params);
+            System.out.println(urlNameString);
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
 
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+//            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
     /**
      * 向指定 URL 发送POST方法的请求
      *
@@ -150,14 +179,7 @@ public class OuterRequestUtil {
 //        content +="&pswd="+URLEncoder.encode("两个个大肥人", "UTF-8");
 //        // DataOutputStream.writeBytes将字符串中的16位的unicode字符以8位的字符形式写到流里面
 //        out.writeBytes(content);
-        String content = new String();
-        for (String key : params.keySet()) {
-            for (String value : params.get(key)) {
-                if (value == null) continue;
-                if (content.length() != 0) content += "&";
-                content += key + "=" + URLEncoder.encode(value, "UTF-8");
-            }
-        }
+        String content = getParamsString(params);
         out.writeBytes(content);
         out.flush();
         out.close();
@@ -171,6 +193,18 @@ public class OuterRequestUtil {
         reader.close();
         connection.disconnect();
         return result;
+    }
+
+    public static String getParamsString(Map<String, String[]> params) throws UnsupportedEncodingException {
+        String content = new String();
+        for (String key : params.keySet()) {
+            for (String value : params.get(key)) {
+                if (value == null) continue;
+                if (content.length() != 0) content += "&";
+                content += key + "=" + URLEncoder.encode(value, "UTF-8");
+            }
+        }
+        return content;
     }
 
     public static void main(String[] args) throws IOException {
